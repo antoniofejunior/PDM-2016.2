f3008c967f9fb4e18eb8e9616c3d5630fece2684
Antonio Junior <antoniofejunior@gmail.com>
2017-04-22 01:38:33 -0300

    gerenciado recurso do flash

--- WebcamPDM/app/src/main/java/pdm/ifpb/webcampdm/WebcamActivity.java
+++ WebcamPDM/app/src/main/java/pdm/ifpb/webcampdm/WebcamActivity.java
@@ -2,24 +2,27 @@ package pdm.ifpb.webcampdm;
 
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.View;
 import android.widget.FrameLayout;
-
-import java.io.IOException;
 
 public class WebcamActivity extends AppCompatActivity {
 
     private Camera camera;
     private CameraPreview cameraPreview;
+    private FloatingActionButton btflash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webcam);
         camera = getCameraInstance();
+        if (camera.getParameters().getFlashMode() == null) {
+            btflash = (FloatingActionButton) findViewById(R.id.fab);
+            btflash.setVisibility(View.INVISIBLE);
+            btflash.setClickable(false);
+        }
         cameraPreview = new CameraPreview(this, camera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.surfaceView);
         preview.addView(cameraPreview);
@@ -34,5 +37,12 @@ public class WebcamActivity extends AppCompatActivity {
         return c;
     }
 
+    public void flash(View view) {
+        if (camera.getParameters().getFlashMode() == Camera.Parameters.FLASH_MODE_OFF) {
+            camera.getParameters().setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        } else {
+            camera.getParameters().setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        }
+    }
 
 }
