388250778a5a09370f99d21eb18b0e71dd00b60f
Antonio Junior <antoniofejunior@gmail.com>
2017-04-22 00:22:53 -0300

    correções

--- WebcamPDM/app/src/main/java/pdm/ifpb/webcampdm/WebcamActivity.java
+++ WebcamPDM/app/src/main/java/pdm/ifpb/webcampdm/WebcamActivity.java
@@ -25,66 +25,14 @@ public class WebcamActivity extends AppCompatActivity {
         preview.addView(cameraPreview);
     }
 
-    public static Camera getCameraInstance(){
+    public static Camera getCameraInstance() {
         Camera c = null;
         try {
-            c = Camera.open(); // attempt to get a Camera instance
+            c = Camera.open();
+        } catch (Exception e) {
         }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
+        return c;
     }
-//
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        if (camera != null) {
-//            camera.release();
-//        }
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//
-//        if (camera != null) {
-//            camera.stopPreview();
-//        }
-//    }
 
-//    @Override
-//    public void surfaceCreated(SurfaceHolder holder) {
-//        try {
-//            camera.setPreviewDisplay(holder);
-//            camera.startPreview();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//    @Override
-//    public void surfaceChanged(SurfaceHolder holder, int format,
-//                               int width, int height) {
-//
-//        if (holder.getSurface() != null) {
-//            try {
-//                camera.stopPreview();
-//            } catch (Exception e) {
-//            }
-//
-//            try {
-//                camera.setPreviewDisplay(holder);
-//                camera.startPreview();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void surfaceDestroyed(SurfaceHolder holder) {
-//    }
 
 }
